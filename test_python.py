4355068dcc689c7f3749367c941f445ee366034f
PedroToma <113950175+Pedro-Toma@users.noreply.github.com>
2025-04-11 07:55:08 -0300

    adição de erro no test_python.py

--- test_python.py
+++ test_python.py
@@ -3,7 +3,7 @@ from python_CI_CD import *
 def test_add():
     assert add(5,2) == 7
     assert add(2,0) == 2
-    assert add(0,0) == 0
+    assert add(0,0) == -1
     assert add(2,-5) == -3
     assert add(-1,-2) == -3
 
